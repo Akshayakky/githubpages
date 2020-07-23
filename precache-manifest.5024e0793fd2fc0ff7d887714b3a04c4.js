36e391c6bee983b691740f7da943608ab9616053
akshayakky <akshaysmpt01@gmail.com>
2020-07-23 14:45:30 +0530

    Updates

---
+++ precache-manifest.5024e0793fd2fc0ff7d887714b3a04c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59870ad1b68b3b128e4c0715445cb46",
+    "url": "/githubpages/index.html"
+  },
+  {
+    "revision": "7d539d2d6da7238649b3",
+    "url": "/githubpages/static/css/main.8917f35e.chunk.css"
+  },
+  {
+    "revision": "40bd88aeb93a4e847396",
+    "url": "/githubpages/static/js/2.96b31dfd.chunk.js"
+  },
+  {
+    "revision": "9f9f1911691b9583d02cd314eccd1cd4",
+    "url": "/githubpages/static/js/2.96b31dfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d539d2d6da7238649b3",
+    "url": "/githubpages/static/js/main.bb837954.chunk.js"
+  },
+  {
+    "revision": "374802a235f052597716",
+    "url": "/githubpages/static/js/runtime-main.5716bee6.js"
+  },
+  {
+    "revision": "ccb5f5aa7410f72daea146da37692d4a",
+    "url": "/githubpages/static/media/cracker3.ccb5f5aa.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/githubpages/precache-manifest.bec8bd7c15508fb8f991da01f9a237be.js"
+  "/githubpages/precache-manifest.5024e0793fd2fc0ff7d887714b3a04c4.js"
 );
 
 self.addEventListener('message', (event) => {
